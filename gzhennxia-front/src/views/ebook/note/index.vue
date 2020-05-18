ad159a5099a54ee7d6633dedb99dd975512b7fec
bli <bli@skystartrade.com>
2020-05-18 19:57:10 +0800

    update-5

--- gzhennxia-front/src/views/ebook/note/index.vue
+++ gzhennxia-front/src/views/ebook/note/index.vue
@@ -1,15 +1,33 @@
 <template>
     <div>
-        <h1>Here is note.</h1>
-        <div width="80%" style=" margin: 0 auto;">
-            <div style="display: flex;justify-content: space-between; width: 80%;margin: 0 auto;">
-                <div>2020-05-11 14:38</div>
-                <div>POS</div>
+        <h1 style="position: fixed;top: 0px;left: 0px;margin: 0px auto;width: 100%;height: 3.6rem;line-height: 3.6rem;">
+            Here is note.
+        </h1>
+        <div>
+            <div style="width: 20rem;position: fixed;margin: 0;top: 3.6rem;left: 0;bottom: 0;box-sizing: border-box;background-color: #fff;border-right: 1px solid #eaecef;">
+                <ul>
+                    <li>menu 1</li>
+                    <li>menu 2</li>
+                    <li>menu 3</li>
+                </ul>
+            </div>
+            <div style="padding-left: 20rem;">
+                <div style="display: flex;justify-content: space-between; width: 80%;margin: 0 auto;">
+                    <div>2020-05-11 14:38</div>
+                    <div>POS</div>
+                    <div>前缀文字，选择的部分，后缀文字</div>
+                </div>
             </div>
         </div>
-        <div>
-            前缀文字，选择的部分，后缀文字
-        </div>
+        <!--        <div width="80%" style=" margin: 0 auto;">-->
+        <!--            <div style="display: flex;justify-content: space-between; width: 80%;margin: 0 auto;">-->
+        <!--                <div>2020-05-11 14:38</div>-->
+        <!--                <div>POS</div>-->
+        <!--            </div>-->
+        <!--        </div>-->
+        <!--        <div>-->
+        <!--            前缀文字，选择的部分，后缀文字-->
+        <!--        </div>-->
     </div>
 </template>
 
